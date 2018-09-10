30b4c4a72c55647a21f5390815d4912d51fdd2a7
mandychan101 <mandychan@pku.edu.cn>
2018-09-10 10:59:59 -0700

    to category

--- public/bundle.js
+++ public/bundle.js
@@ -17813,11 +17813,11 @@ const playlist = app.service('/playlist');
 const db = app.service('/mongo');
 
 // 0 is scene rating project; 1 is emotion category project
-const Project_version = 'scene';  // ‘scene or category’
+const Project_version = 'category';  // ‘scene or category’
 
-// const personOrContext = Math.round(Math.random()*2); // randome number of 0 or 1 or 2
+const personOrContext = Math.round(Math.random()*2); // randome number of 0 or 1 or 2
 // const personOrContext = Math.round(Math.random()*1); // randome number of 0 or 1 or 2
-const personOrContext = 2
+// const personOrContext = 2
 // const personOrContext = 0;
 const Condition_type = personOrContext === 0 ? 'baseline' : personOrContext === 1 ? 'characterOnly' : 'contextOnly';
 // 0: baseline
