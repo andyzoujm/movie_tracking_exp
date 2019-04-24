3810a892dc153542534f306b9b71a1b02a7db8d9
Zhimin Chen <101mandychan@gmail.com>
2019-04-24 14:35:08 -0700

    link to index-image.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'public/src/index.js'),
+  entry: path.resolve(__dirname, 'public/src/index-image.js'),
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
